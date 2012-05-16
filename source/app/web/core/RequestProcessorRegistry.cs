dfdbf53ad4a7ac22d03d654f0df90e19a78fe9dc
git_name <git_email>
2012-05-16 17:24:37 -0600

    Pushing new changes

--- source/app/web/core/RequestProcessorRegistry.cs
+++ source/app/web/core/RequestProcessorRegistry.cs
@@ -7,18 +7,18 @@ namespace app.web.core
     {
 
         IEnumerable<IProcessOneRequest> all_the_possible_processors;
-        CreateMissingRequestProcessor_Behaviour the_special_case;
+        CreateMissingRequestProcessor_Behaviour create_special_case;
 
-        public RequestProcessorRegistry(IEnumerable<IProcessOneRequest> all_the_possible_processors, CreateMissingRequestProcessor_Behaviour the_special_case)
+        public RequestProcessorRegistry(IEnumerable<IProcessOneRequest> all_the_possible_processors, CreateMissingRequestProcessor_Behaviour create_special_case)
         {
             this.all_the_possible_processors = all_the_possible_processors;
-            this.the_special_case = the_special_case;
+            this.create_special_case = create_special_case;
         }
 
         public IProcessOneRequest get_the_command_that_can_handle(IContainRequestDetails the_request)
         {
             var process_one_request = all_the_possible_processors.FirstOrDefault(processor => processor.can_process(the_request));
-            return process_one_request ?? the_special_case.Invoke(); 
+            return process_one_request ?? create_special_case.Invoke();
         }
     }
 }
