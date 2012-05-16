0d112212cee1efb83635c1bde2bb6f322bcebe45
Geoff Parris <geoff@mega.net>
2012-05-16 15:23:11 -0700

    Pushing new changes

--- source/app/web/core/RequestProcessorRegistry.cs
+++ source/app/web/core/RequestProcessorRegistry.cs
@@ -7,15 +7,17 @@ namespace app.web.core
     {
 
         IEnumerable<IProcessOneRequest> all_the_possible_processors;
+        CreateMissingRequestProcessor_Behaviour default_command;
 
-        public RequestProcessorRegistry(IEnumerable<IProcessOneRequest> all_the_possible_processors)
+        public RequestProcessorRegistry( IEnumerable<IProcessOneRequest> all_the_possible_processors, CreateMissingRequestProcessor_Behaviour defaultCommand )
         {
             this.all_the_possible_processors = all_the_possible_processors;
+            this.default_command = defaultCommand;
         }
 
         public IProcessOneRequest get_the_command_that_can_handle(IContainRequestDetails the_request)
         {
-            return all_the_possible_processors.First(processor => processor.can_process(the_request));
+            return all_the_possible_processors.FirstOrDefault(processor => processor.can_process(the_request)) ?? default_command.Invoke();
         }
     }
 }
