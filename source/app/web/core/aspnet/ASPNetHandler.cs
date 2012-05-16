526c82198372ab161e803de93328ad96e2502625
travismartinjones <travismartinjones@gmail.com>
2012-05-16 16:04:28 -0500

    Pushing new changes

--- source/app/web/core/aspnet/ASPNetHandler.cs
+++ source/app/web/core/aspnet/ASPNetHandler.cs
@@ -6,15 +6,18 @@ namespace app.web.core.aspnet
     public class ASPNetHandler : IHttpHandler
     {
         IProcessWebRequests front_controller;
+        ICreateControllerRequests controller_factory;
 
-        public ASPNetHandler(IProcessWebRequests front_controller)
+        public ASPNetHandler(IProcessWebRequests front_controller, ICreateControllerRequests controller_factory)
         {
             this.front_controller = front_controller;
+            this.controller_factory = controller_factory;
         }
 
         public void ProcessRequest(HttpContext context)
         {
-            throw new NotImplementedException();
+            var response = controller_factory.create_from(context);
+            front_controller.process(response);
         }
 
         public bool IsReusable
---
+++ source/app/web/core/aspnet/HttpResponseFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace app.web.core.aspnet
+{
+    public class HttpResponseFactory : ICreateControllerRequests
+    {
+        public object create_from(HttpContext new_http_context)
+        {
+            // perform transformatino of HttpContext into our response object
+            return new object();
+        }
+    }
+}
