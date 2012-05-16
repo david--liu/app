cac1e33a054a1f60960e25a9e38b523475718e69
laverne kelly <levesque09@gmail.com>
2012-05-16 13:57:07 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace app
 {
     public class Calculator
     {
         public int add(int first, int second)
         {
+            if (first < 0 || second<0)throw new ArgumentException("we cant handle engatives");
             return first + second;
         }
     }
