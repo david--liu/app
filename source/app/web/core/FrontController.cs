f935c2a7545f83a2a2c290ce115ef1cc6db3d6c1
Geoff Parris <geoff@mega.net>
2012-05-16 14:49:45 -0700

    Pushing new changes

--- source/app/web/core/FrontController.cs
+++ source/app/web/core/FrontController.cs
@@ -1,10 +1,14 @@
 ﻿namespace app.web.core
 {
-    public class FrontController : IProcessWebRequests
-    {
-        public void process(IContainRequestDetails a_new_request)
-        {
-            throw new System.NotImplementedException();
+    public class FrontController : IProcessWebRequests{
+        IFindRequestProcessors request_processor_locator;
+
+        public FrontController(IFindRequestProcessors requestProcessorLocator){
+            request_processor_locator = requestProcessorLocator;
+        }
+
+        public void process(IContainRequestDetails a_new_request){
+            request_processor_locator.get_the_command_that_can_handle(a_new_request).process(a_new_request);
 
         }
     }
