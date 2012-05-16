8bcabda5b61bb93ac6e9737a60931c2ffbaf4fc4
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-05-15 18:09:43 -0600

    Updating the scripts for git

--- git_utils.rb
+++ git_utils.rb
@@ -1,8 +1,12 @@
 def exit_if_on_the_master_branch
+  exit_if_on_branch('master')
+end
+
+def exit_if_on_branch(branch)
   status = `git branch`
 
-  if (/\* master/ =~ status)
-    puts "You need to run this on a non master branch"
+  if (%r/\* #{branch}/ =~ status)
+    puts "You cant run this command on the branch #{branch}"
     exit
   end
 end
@@ -12,12 +16,12 @@ def checkout(branch_name)
   `git checkout #{branch_name}`
 end
 
-def exit_if_not_on_the_master_branch
+def exit_if_not_on_the_branch(branch)
 
   status = `git branch`
 
-  if (/\* master/ !~ status)
-    puts "You need to run this on a master branch"
+  if (%r/\* #{branch}/ !~ status)
+    puts "You need to run this on the #{branch} branch"
     exit
   end
 
