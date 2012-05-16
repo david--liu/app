2a5057b53c3eb7438fb80ce4b0d5c8b248e7d8dc
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-05-16 16:37:32 -0600

    Updated

--- source/app/web/core/RequestProcessorRegistry.cs
+++ source/app/web/core/RequestProcessorRegistry.cs
@@ -5,19 +5,20 @@ namespace app.web.core
 {
     public class RequestProcessorRegistry : IFindRequestProcessors
     {
-
         IEnumerable<IProcessOneRequest> all_the_possible_processors;
-        CreateMissingRequestProcessor_Behaviour default_command;
+        CreateMissingRequestProcessor_Behaviour special_case_factory;
 
-        public RequestProcessorRegistry( IEnumerable<IProcessOneRequest> all_the_possible_processors, CreateMissingRequestProcessor_Behaviour defaultCommand )
+        public RequestProcessorRegistry(IEnumerable<IProcessOneRequest> all_the_possible_processors,
+                                        CreateMissingRequestProcessor_Behaviour special_case_factory)
         {
             this.all_the_possible_processors = all_the_possible_processors;
-            this.default_command = defaultCommand;
+            this.special_case_factory = special_case_factory;
         }
 
         public IProcessOneRequest get_the_command_that_can_handle(IContainRequestDetails the_request)
         {
-            return all_the_possible_processors.FirstOrDefault(processor => processor.can_process(the_request)) ?? default_command.Invoke();
+            return all_the_possible_processors.FirstOrDefault(processor => processor.can_process(the_request)) ??
+                special_case_factory();
         }
     }
 }
