a119b09bf2272257f1642c96d42141a0928a0dd4
git_name <git_email>
2012-05-16 14:24:20 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,13 +1,24 @@
 using System;
+using System.Data;
 using System.Linq;
 
 namespace app
 {
     public class Calculator
     {
+
+        IDbConnection connection;
+
+        public Calculator(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+
         public int add(int first, int second)
         {
             ensure_all_are_positive(first,second);
+
+            connection.Open();
             return first + second;
         }
 
