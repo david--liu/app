2e22ec394cc14930a9e1548671348f4d8058d7b1
git_name <git_email>
2012-05-16 16:43:53 -0600

    Pushing new changes

--- source/app/web/core/FrontController.cs
+++ source/app/web/core/FrontController.cs
@@ -2,10 +2,17 @@
 {
     public class FrontController : IProcessWebRequests
     {
+        IFindRequestProcessors command_registry;
+
+        public FrontController(IFindRequestProcessors command_registry)
+        {
+            this.command_registry = command_registry;
+        }
+
         public void process(IContainRequestDetails a_new_request)
         {
-            throw new System.NotImplementedException();
-
+            var the_command_that_can_handle = command_registry.get_the_command_that_can_handle(a_new_request);
+            the_command_that_can_handle.process(a_new_request);
         }
     }
 }
