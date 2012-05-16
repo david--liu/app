bdc49b531f890cee751d66f19680d4617a101e0d
git_name <git_email>
2012-05-16 17:10:09 -0600

    Pushing new changes

--- source/app/web/core/RequestProcessorRegistry.cs
+++ source/app/web/core/RequestProcessorRegistry.cs
@@ -1,10 +1,26 @@
-﻿namespace app.web.core
+﻿using System.Collections.Generic;
+
+namespace app.web.core
 {
     public class RequestProcessorRegistry : IFindRequestProcessors
     {
+
+        IEnumerable<IProcessOneRequest> all_the_possible_processors;
+
+        public RequestProcessorRegistry(IEnumerable<IProcessOneRequest> all_the_possible_processors)
+        {
+            this.all_the_possible_processors = all_the_possible_processors;
+        }
+
         public IProcessOneRequest get_the_command_that_can_handle(IContainRequestDetails the_request)
         {
-            throw new System.NotImplementedException();
+            foreach (var processor in all_the_possible_processors)
+            {
+                if (processor.can_process(the_request))
+                    return processor;
+            }
+
+            return null;
         }
     }
 }
