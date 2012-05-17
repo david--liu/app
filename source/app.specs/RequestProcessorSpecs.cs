ef955f67606fec259104a0edc29a6259943476a7
laverne kelly <levesque09@gmail.com>
2012-05-16 18:36:18 -0600

    Pushing new changes

---
+++ source/app.specs/RequestProcessorSpecs.cs
@@ -0,0 +1,40 @@
+﻿ using Machine.Specifications;
+ using developwithpassion.specifications.rhinomocks;
+ using developwithpassion.specifications.extensions;
+ using app.web.core;
+
+namespace app.specs
+{  
+    [Subject(typeof(RequestProcessor))]  
+    public class RequestProcessorSpecs
+    {
+        public abstract class concern : Observes<IProcessOneRequest,
+                                            RequestProcessor>
+        {
+        
+        }
+
+
+        public class when_asked_if_you_can_process_a_request : concern
+        {
+            Establish c = () =>
+            {
+                fake.an<IContainRequestDetails>();
+            };
+
+            Because b = () =>
+            {
+                response = sut.can_process(request);
+            };
+
+
+            It should_return_true_when_the_request_can_be_processed = () =>
+            {
+                response.ShouldBeTrue();
+            };
+
+            static bool response;
+            static IContainRequestDetails request;
+        }
+    }
+}
---
+++ source/app/web/core/RequestProcessor.cs
@@ -0,0 +1,15 @@
+﻿namespace app.web.core
+{
+    public class RequestProcessor : IProcessOneRequest
+    {
+        public void process(IContainRequestDetails the_request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool can_process(IContainRequestDetails request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
