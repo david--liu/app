f5d5cdd2e7f236bd4246058ab62f25f446df3f93
travismartinjones <travismartinjones@gmail.com>
2012-05-18 15:17:49 -0500

    Pushing new changes

--- source/app/infrastructure/containers/simple/DependencyContainer.cs
+++ source/app/infrastructure/containers/simple/DependencyContainer.cs
@@ -1,16 +1,32 @@
-﻿namespace app.infrastructure.containers.simple
+﻿using System;
+
+namespace app.infrastructure.containers.simple
 {
     public class DependencyContainer : IFetchDependencies
     {
         IFindAFactoryForADependency factories;
+        readonly ItemCreationExceptionFactory_Behaviour exception_handing_behavior;
 
-        public DependencyContainer(IFindAFactoryForADependency factories){
+        public DependencyContainer(IFindAFactoryForADependency factories, ItemCreationExceptionFactory_Behaviour exception_handing_behavior)
+        {
             this.factories = factories;
+            this.exception_handing_behavior = exception_handing_behavior;
         }
 
         public Collaborator an<Collaborator>()
         {
-            return (Collaborator)factories.get_factory_that_can_create(typeof(Collaborator)).create();
+            Collaborator dependency;
+
+            try
+            {
+                dependency = (Collaborator) factories.get_factory_that_can_create(typeof(Collaborator)).create();
+            }
+            catch(Exception ex)
+            {
+                throw exception_handing_behavior(typeof(Collaborator),ex);
+            }
+
+            return dependency;
         }
     }
 }
