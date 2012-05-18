46dd1ae64466c28492d5174703a61506faa7cea3
Geoff Parris <geoff@mega.net>
2012-05-18 13:08:26 -0700

    Pushing new changes

--- source/app/infrastructure/containers/simple/DependencyContainer.cs
+++ source/app/infrastructure/containers/simple/DependencyContainer.cs
@@ -2,9 +2,15 @@
 {
     public class DependencyContainer : IFetchDependencies
     {
+        IFindAFactoryForADependency factory_finder;
+
+        public DependencyContainer(IFindAFactoryForADependency factoryFinder){
+            factory_finder = factoryFinder;
+        }
+
         public Collaborator an<Collaborator>()
         {
-            throw new System.NotImplementedException();
+            return (Collaborator)factory_finder.get_factory_that_can_create(typeof(Collaborator)).create();
         }
     }
 }
